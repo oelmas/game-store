100163de41d045779a395f963616f4137bc14ff2
Öner ELMAS <oelmas@gmail.com>
2026-01-26 21:48:59 +0300

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from '@chakra-ui/react'
-
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
-
   return (
-        <Button colorPalette='blue'>Click me</Button>
+    <Grid
+      templateColumns={{ base: '1fr', lg: '200px 1fr' }}
+      templateRows={{ base: 'auto 1fr', lg: '60px 1fr' }}
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      h="100vh"
+      
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="dodgerblue" display={{ base: 'none', lg: 'block' }}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="limegreen">
+        Main
+      </GridItem>
+    </Grid>
   )
 }
 
