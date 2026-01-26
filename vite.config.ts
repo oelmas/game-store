802617f19acef2c331861e5e7ffee937014ed9c6
Öner ELMAS <oelmas@gmail.com>
2026-01-26 20:51:33 +0300

    Update dependencies and refactor App component to use Chakra UI. Remove App.css and index.css, and integrate vite-tsconfig-paths for improved path resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
+      jsxRuntime: 'automatic',
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [],
       },
     }),
+    tsconfigPaths(),
   ],
+  optimizeDeps: {
+    include: ['@chakra-ui/react'],
+  },
 })
