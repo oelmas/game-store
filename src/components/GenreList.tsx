0717582fcbed1881180bbe91db914ec8d613c77f
Öner ELMAS <oelmas@gmail.com>
2026-01-29 14:36:55 +0300

    Update GenreList component to display genres with images using Chakra UI List and Image components
    Refactor GenreList to replace the unordered list with Chakra UI's List and ListItem components. Integrate getCroppedImageUrl for displaying genre images, enhancing the visual presentation of the genre list.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,6 @@
-import { Spinner } from "@chakra-ui/react";
+import { List, ListItem, Spinner, Image } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
 	const { data: genres, error, isLoading } = useGenres();
@@ -8,11 +9,19 @@ const GenreList = () => {
 	if (isLoading) return <Spinner />;
 
 	return (
-		<ul>
+		<List>
 			{genres.map((genre) => (
-				<li key={genre.id}>{genre.name}</li>
+				<ListItem key={genre.id}>
+					<Image
+						src={getCroppedImageUrl(genre.image_background)}
+						objectFit="cover"
+						boxSize="42px"
+						borderRadius="8px"
+					/>
+					{genre.name}
+				</ListItem>
 			))}
-		</ul>
+		</List>
 	);
 };
 
