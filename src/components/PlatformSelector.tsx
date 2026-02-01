021802ba5afa41cae377dcafe75ad19b1bc62191
Öner ELMAS <oelmas@gmail.com>
2026-02-01 14:11:11 +0300

    Add genres and platforms data files with initial content
    - Introduced genres.ts and platforms.ts files containing structured data for game genres and platforms.
    - Updated useGenres and usePlatforms hooks to return static data instead of fetching from an API, improving performance and reducing loading times.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,35 +4,35 @@ import usePlatforms from "../hooks/usePlatforms";
 import type { Platform } from "../hooks/useGames";
 
 interface Props {
-  onSelectPlatform: (platform: Platform | null) => void;
-  selectedPlatform: Platform | null;
+	onSelectPlatform: (platform: Platform | null) => void;
+	selectedPlatform: Platform | null;
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data: platforms, error, isLoading } = usePlatforms();
+	const { data: platforms, error, isLoading } = usePlatforms();
 
-  if (error) return null;
-  if (isLoading) return <div>Loading...</div>;
+	if (error) return null;
+	if (isLoading) return <div>Loading...</div>;
 
-  return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platform"}
-      </MenuButton>
-      <MenuList>
-        {platforms.map((platform) => {
-          return (
-            <MenuItem
-              key={platform.id}
-              onClick={() => onSelectPlatform(platform)}
-            >
-              {platform.name}
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Menu>
-  );
+	return (
+		<Menu>
+			<MenuButton as={Button} rightIcon={<BsChevronDown />}>
+				{selectedPlatform?.name || "Platform"}
+			</MenuButton>
+			<MenuList>
+				{platforms.map((platform) => {
+					return (
+						<MenuItem
+							key={platform.id}
+							onClick={() => onSelectPlatform(platform)}
+						>
+							{platform.name}
+						</MenuItem>
+					);
+				})}
+			</MenuList>
+		</Menu>
+	);
 };
 
 export default PlatformSelector;
--- src/theme.ts
+++ src/theme.ts
@@ -6,4 +6,21 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 };
 
-export default extendTheme({ config });
+export default extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#a0a0a0",
+      500: "#898989",
+      600: "#6c6c6c",
+
+    },
+    700: "#5c5c5c",
+    800: "#4a4a4a",
+    900: "#3b3b3b",
+  },
+});
