71b7b8e9ad9bb1521baff534cc84850745551957
Öner ELMAS <oelmas@gmail.com>
2026-02-01 13:15:41 +0300

    Add genres and platforms data files with initial content
    - Introduced genres.ts and platforms.ts files containing structured data for game genres and platforms.
    - Updated useGenres and usePlatforms hooks to return static data instead of fetching from an API, improving performance and reducing loading times.

---
+++ src/data/platforms.ts
@@ -0,0 +1,18 @@
+
+
+export default [
+  { id: 1, name: "PC", slug: "pc" },
+  { id: 2, name: "PlayStation", slug: "playstation" },
+  { id: 3, name: "Xbox", slug: "xbox" },
+  { id: 4, name: "iOS", slug: "ios" },
+  { id: 5, name: "Mac", slug: "mac" },
+  { id: 6, name: "Linux", slug: "linux" },
+  { id: 7, name: "Nintendo", slug: "nintendo" },
+  { id: 8, name: "Android", slug: "android" },
+  { id: 9, name: "Atari", slug: "atari" },
+  { id: 10, name: "Commodore", slug: "commodore" },
+  { id: 11, name: "Sega", slug: "sega" },
+  { id: 12, name: "Sony", slug: "sony" },
+  { id: 13, name: "Other", slug: "other" },
+  { id: 14, name: "Web", slug: "web" },
+];
